f2d959b87745134d2e869ba97b0d5fdf2b2d189d
geeonie <nomad.jiwhan@gmail.com>
2022-02-07 18:38:27 +0900

    refactor: changed function name in mailController

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { postTrash, deleteTrash } = require("../controllers/mailController");
+const { moveToTrash, deleteTrash } = require("../controllers/mailController");
 
 const router = express.Router();
 
-router.post("/:id/mails/trash", postTrash);
+router.post("/:id/mails/trash", moveToTrash);
 router.delete("/:id/mails/trash", deleteTrash);
 
 module.exports = router;
