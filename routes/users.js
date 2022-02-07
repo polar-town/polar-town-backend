80091d91cc080a990135c84cbe868c13d3a24b58
geeonie <nomad.jiwhan@gmail.com>
2022-02-07 11:37:51 +0900

    chore: revised endpoint of users route

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,11 @@ router.get("/:id/mails/:inBoxId", getMailList);
 
 router.post("/:id/mails/trash", moveToTrash);
 
+<<<<<<< HEAD
 router.delete("/:id/mails/trash", deleteTrash);
+=======
+router.post("/:id/mails/trash", handleTrash);
+router.delete("/:id/mails/trash", emptyTrash);
+>>>>>>> 2eec934 (chore: revised endpoint of users route)
 
 module.exports = router;
