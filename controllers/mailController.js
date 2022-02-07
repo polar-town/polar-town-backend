8dc5dac1c7c2dcbe93976e41ca47a587b4d1a7de
geeonie <nomad.jiwhan@gmail.com>
2022-02-07 12:21:48 +0900

    feature: implemented postTrash function

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,23 +1,31 @@
 const createError = require("http-errors");
 const axios = require("axios");
 
-const handleTrash = async (req, res, next) => {
+const postTrash = (req, res, next) => {
   const { gapiauthorization } = req.headers;
-  const accessToken = gapiauthorization.split(" ")[1];
   const { mailId } = req.body;
 
-  const test = axios.post(
-    "https://gmail.googleapis.com/gmail/v1/users/me/messages/17ed1e55206fde08/trash",
-    null,
-    {
-      headers: { Authorization: accessToken },
+  mailId.forEach(async (id) => {
+    try {
+      await axios.post(
+        `https://gmail.googleapis.com/gmail/v1/users/me/messages/${id}/trash`,
+        null,
+        {
+          headers: { Authorization: gapiauthorization },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      next(error);
     }
-  );
+  });
+
+  res.send({ result: "ok" });
 };
 
-const emptyTrash = async (req, res, next) => {
+const deleteTrash = async (req, res, next) => {
   const { gapiauthorization } = req.headers;
   const accessToken = gapiauthorization.split(" ")[1];
 };
 
-module.exports = { handleTrash, emptyTrash };
+module.exports = { postTrash, deleteTrash };
