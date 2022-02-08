5c9e8fdc715945d0a9e3318fca2d716ccb75c2bc
choyeun98 <choyeun98@naver.com>
2022-02-08 21:50:09 +0900

    feature: /users/:id/guestBook GET

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const getUserInfo = async (req, res, next) => {
 
   try {
     const user = await User.findById(id);
+
     res.json({
       user,
     });
@@ -14,6 +15,20 @@ const getUserInfo = async (req, res, next) => {
   }
 };
 
+const getGuestBook = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const userGuestBook = await User.findById(id).guestBook.exec();
+
+    res.json(userGuestBook);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
 module.exports = {
   getUserInfo,
+  getGuestBook,
 };
