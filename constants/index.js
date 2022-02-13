7b4db654646ee922d05f0b5dee8b230b8108a2a3
geeonie <nomad.jiwhan@gmail.com>
2022-02-14 02:44:14 +0900

    feature : user left logic implemented

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+const EVENTS = {
+  JOIN: "join",
+  LEFT: "left",
+  GET_MESSAGES: "getMessage",
+  SEND_MESSAGE: "sendMessage",
+  DISCONNECT: "disconnect",
+};
+
+const TYPE = {
+  SIGNOUT: "signout",
+  TRANSITION: "transition",
+};
+
+module.exports = { EVENTS, TYPE };
